77d6f602dd35a9c5998cdaafc13a59a1663dc92e
Rephrain Archaimeric <LTL003096@lautan-luas.com>
2026-03-27 13:56:40 +0700

    Fix CSS

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import AppShell from "$lib/components/Layout/AppShell.svelte";
 	import Toast from "$lib/components/UI/Toast.svelte";
-	import "../../app.css";
 
 	export let data;
 	$: user = data?.user;
